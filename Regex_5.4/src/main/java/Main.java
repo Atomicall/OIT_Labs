d6dfea2494f41898353866adde0137438587874d
vnu <vnullo@gmail.com>
2022-11-01 13:52:15 +0300

    5.4 small changes

--- Regex_5.4/src/main/java/Main.java
+++ Regex_5.4/src/main/java/Main.java
@@ -6,9 +6,14 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String PHONE_NUMBER_REGEXP = "^\\([0-9]{3}\\)\\ [0-9]{3}-[0-9]{4}$";
     private static final String SOCIAL_NUMBER_REGEXP = "^[0-9]{3}-[0-9]{2}-[0-9]{4}$";
-    private static final String DATE_REGEXP = "^(January|February|March|April|May|June|July|August|September|October|November|December)\\ (0[1-9]|(1|2)[0-9]|3(0|1)),\\ [0-9]{4}$";
+    private static final String DATE_REGEXP = "^(January|February|March|April|May|June|July|August|September|October|November|December)\\ (0[1-9]|(1|2)[0-9]|30|31),\\ [0-9]{4}$";
     private static final String IP_ADDRESS_REGEXP = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\\.(?!$)|$)){4}$";
     private static final String CAR_NUMBER_REGEXP = "^[0-9]{4}[A-ZА-Я5]{2}$";
+    private static final Pattern phoneNumberRegexPattern = Pattern.compile(PHONE_NUMBER_REGEXP);
+    private static final Pattern socialNumberRegexPattern = Pattern.compile(SOCIAL_NUMBER_REGEXP);
+    private static final Pattern dateRegexPattern = Pattern.compile(DATE_REGEXP);
+    private static final Pattern ipAddressRegexPattern = Pattern.compile(IP_ADDRESS_REGEXP);
+    private static final Pattern carNumberRegexPattern = Pattern.compile(CAR_NUMBER_REGEXP);
 
     private static void printMenu() {
         System.out.println("Check input string with pattern:");
@@ -21,23 +26,22 @@ public class Main {
         System.out.print(">");
     }
 
-    private static void checkRegex(String regex) {
+    private static void checkRegex(Pattern pattern) {
         System.out.println("0.Return\nEnter line:");
         while (true) {
             System.out.print(">");
             String inputString = scanner.nextLine();
             if (inputString.equals("0")) break;
-            System.out.println("Is valid string? " + (checkMatch(inputString.trim(), regex) ? "TRUE" : "FALSE"));
+            System.out.println("Is valid string? " + (checkMatch(inputString.trim(), pattern) ? "TRUE" : "FALSE"));
         }
     }
 
-    private static boolean checkMatch(String inputString, String regex) {
+    private static boolean checkMatch(String inputString, Pattern pattern) {
         if (inputString.isEmpty()) {
             System.out.println("Input string is empty");
             return false;
         }
         boolean hasMatch = false;
-        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(inputString);
         while (matcher.find()) {
             hasMatch = true;
@@ -52,19 +56,19 @@ public class Main {
             String inputString = scanner.nextLine();
             switch (inputString) {
                 case "1" -> {
-                    checkRegex(PHONE_NUMBER_REGEXP);
+                    checkRegex(phoneNumberRegexPattern);
                 }
                 case "2" -> {
-                    checkRegex(SOCIAL_NUMBER_REGEXP);
+                    checkRegex(socialNumberRegexPattern);
                 }
                 case "3" -> {
-                    checkRegex(DATE_REGEXP);
+                    checkRegex(dateRegexPattern);
                 }
                 case "4" -> {
-                    checkRegex(IP_ADDRESS_REGEXP);
+                    checkRegex(ipAddressRegexPattern);
                 }
                 case "5" -> {
-                    checkRegex(CAR_NUMBER_REGEXP);
+                    checkRegex(carNumberRegexPattern);
                 }
                 case "0" -> {
                     System.exit(0);
